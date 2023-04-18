938b83f0433cc744c4f7194a682f81a245c417ff
Ann Zhou <ann.zhou@wustl.edu>
2023-04-18 15:58:02 -0400

    sync

---
+++ preprocess/visualize.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class RawDataVisualization:
+    def __init__(self) -> None:
+        pass
+
+    def load(self):
+        pass
+
+    def parse(self):
+        # Remove queues with queue_len = 0 throughout
+        pass
+    
+    def plot(self):
+        pass
+
+if __name__ == "__main__":
+    visualizer = RawDataVisualization()
