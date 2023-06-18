62c4c5d2b0fa3488d2d5d0f51d60b3a15924bc23
Ann Zhou <ann.zhou@princeton.edu>
2023-06-18 13:48:33 +0000

    [star-topology] add star_simple_plot.py

---
+++ plots/star_simple_plot.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dir = "/u/az6922/data/"
+file = dir + "tor-single-2-101-0.stat"
+
+#df = pd.read_csv(file, delim_whitespace=True)
+
+
+def plot_port10():
+	qsize_list = list()
+	buffer_list = list()
+	time_list = list()
+	with open(file) as f:
+		line_count = -1
+		for line in f:
+			line_count += 1
+			if line_count <= 0: continue
+			array = [x for x in line.split()]
+			buffer_list.append(float(array[2])*float(array[1])*10000)
+			time_list.append(int(array[0])-2000000000)
+			qsize_list.append(int(array[53]))
+
+	#plt.plot(time_list[:1000000],buffer_list[:1000000],label="occupied buffer")
+	plt.plot(time_list,buffer_list,label="occupied buffer")
+	#plt.plot(time_list[:1000000],qsize_list[:1000000],label="port0 qsize")
+	plt.plot(time_list,qsize_list,label="port10 qsize")
+	plt.title("port10 qsize over time")
+	plt.legend()
+	plt.savefig(dir+"star_simple_plot_port10.pdf")
+
+
+def plot_all(numqueues):
+	qsize_list_arr = []
+	for i in range(numqueues):
+		qsize_list_arr.append(list())
+	buffer_list = list()
+	time_list = list()
+	with open(file) as f:
+		line_count = -1
+		for line in f:
+			line_count += 1
+			if line_count <= 0: continue
+			array = [x for x in line.split()]
+			buffer_list.append(float(array[2])*float(array[1])*10000)
+			time_list.append(int(array[0])-2000000000)
+			for i in range(numqueues):
+				qsize_list_arr[i].append(int(array[3+5*i]))
+
+	#plt.plot(time_list[:1000000],buffer_list[:1000000],label="occupied buffer")
+	plt.plot(time_list,buffer_list,label="occupied buffer")
+	#plt.plot(time_list[:1000000],qsize_list[:1000000],label="port0 qsize")
+	for i in range(numqueues):
+		plt.plot(time_list,qsize_list_arr[i],label="port"+str(i)+" qsize")
+	plt.title("buffer & qsize of all ports over time")
+	plt.legend()
+	plt.savefig(dir+"star_simple_plot_all.pdf")
+
+
+def plot_buffer():
+	buffer_list = list()
+	time_list = list()
+	with open(file) as f:
+		line_count = -1
+		for line in f:
+			line_count += 1
+			if line_count<=0: continue
+			array = [x for x in line.split()]
+			buffer_list.append(float(array[2])*float(array[1])*10000)
+			time_list.append(int(array[0])-2000000000)
+
+	plt.plot(time_list,buffer_list,label="occupied buffer")
+	plt.title("occupied buffer over time")
+	plt.legend()
+	plt.savefig(dir+"star_simple_plot_buffer.pdf")
+
+
+if __name__ == "__main__":
+	plot_port10()
