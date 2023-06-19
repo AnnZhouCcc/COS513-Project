18cb22b26bfec17886524c4f31b8d52115183aa1
Ann Zhou <ann.zhou@princeton.edu>
2023-06-19 01:57:06 +0000

    add plot_generic()

--- plots/star_simple_plot.py
+++ plots/star_simple_plot.py
@@ -74,6 +74,32 @@ def plot_buffer():
 	plt.legend()
 	plt.savefig(dir+"star_simple_plot_buffer.pdf")
 
+# sentbytes: plot_generic(10, 2, "sentbytes")
+# droppedbytes: plot_generic(10,3,"droppedbytes")
+def plot_generic(numqueues, offset, name):
+	qsize_list_arr = []
+	for i in range(numqueues):
+		qsize_list_arr.append(list())
+	buffer_list = list()
+	time_list = list()
+	with open(file) as f:
+		line_count = -1
+		for line in f:
+			line_count += 1
+			if line_count <= 0: continue
+			array = [x for x in line.split()]
+			buffer_list.append(float(array[2])*float(array[1])*10000)
+			time_list.append(int(array[0])-2000000000)
+			for i in range(numqueues):
+				qsize_list_arr[i].append(int(array[3+5*i+offset]))
+
+	#plt.plot(time_list,buffer_list,label="occupied buffer")
+	for i in range(numqueues):
+		plt.plot(time_list,qsize_list_arr[i],label="port"+str(i)+" "+name)
+	plt.title(name+" of all ports over time")
+	plt.legend()
+	plt.savefig(dir+"star_simple_plot_"+name+".pdf")
+
 
 if __name__ == "__main__":
-	plot_port10()
+	plot_generic(10,3,"droppedbytes")
