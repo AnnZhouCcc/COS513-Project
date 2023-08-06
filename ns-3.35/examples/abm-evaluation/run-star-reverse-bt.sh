15f51db38ce32f05ea345e8b68219177a718f3c7
Ann Zhou <ann.zhou@princeton.edu>
2023-08-06 23:26:40 +0000

    [reverse-bt] fix & sync run-star-reverse-bt.sh

--- ns-3.35/examples/abm-evaluation/run-star-reverse-bt.sh
+++ ns-3.35/examples/abm-evaluation/run-star-reverse-bt.sh
@@ -3,6 +3,8 @@ NS3="/u/az6922/COS513-Project/ns-3.35"
 DIR="$NS3/examples/abm-evaluation"
 DUMP_DIR="/u/az6922/data"
 
+cd $NS3
+
 DT=101
 FAB=102
 CS=103
@@ -18,18 +20,12 @@ DCTCP=2
 # TIMELY=6
 # THETAPOWERTCP=7
 
-# BUF_ALGS=($DT $FAB $CS $IB $ABM)
-# TCP_ALGS=($CUBIC $DCTCP $TIMELY $POWERTCP)
+# BTMODE
+BURSTWITHCONT=0
+BURSTONLY=1
+CONTONLY=2
 
-# SERVERS=32
-# LEAVES=2
-# SPINES=2
-# LINKS=4
-# SERVER_LEAF_CAP=1
-# LEAF_SPINE_CAP=1
-# LATENCY=10
-
-SERVER_LEAF_CAP=1
+SERVER_LEAF_CAP=10
 LEAF_SINK_CAP=1
 SERVER_LEAF_LATENCY=10000
 LEAF_SINK_LATENCY=10000
@@ -43,34 +39,18 @@ ALPHAFILE="$DIR/alphas"
 CDFFILE="$DIR/websearch.txt"
 CDFNAME="WS"
 
-# ALPHA_UPDATE_INT=1 # 1 RTT
-
 NUMSINKS=2
 NUMNODES=11
 
-# STATIC_BUFFER=0
-# BUFFER=$(( 1000*1000*9  ))
-#BUFFER_PER_PORT_PER_GBPS=0.5 #9.6 # https://baiwei0427.github.io/papers/bcc-ton.pdf (Trident 2)
-# BUFFER=$(python3 -c "print(int($BUFFER_PER_PORT_PER_GBPS*1024*($SERVERS+$LINKS*$SPINES)*$SERVER_LEAF_CAP))")
-# BUFFER=$(python3 -c "print(int($BUFFER_PER_PORT_PER_GBPS*1024*($NUMNODES*$SERVER_LEAF_CAP+$NUMSINKS*$LEAF_SINK_CAP)))")
-BUFFER=4000000
+BUFFER=7000000
 
 START_TIME=2
 END_TIME=12
 FLOW_END_TIME=10
 
-
-cd $NS3
-
-
-# 20 exps
-# BURST_SIZES=0.9
-# BURST_SIZE=$(python3 -c "print($BURST_SIZES*$BUFFER)")
-# BURST_FREQ=3
-
 TCP=$CUBIC
 ALG=$DT
-version=0
+version=7
 
 CONTA=1
 BURSTA=8
@@ -78,23 +58,14 @@ BURSTA=8
 NUMCONTFLOWS=1
 NUMBURSTFLOWS=10
 
-# BTMODE
-BURSTWITHCONT=0
-BURSTONLY=1
-CONTONLY=2
-BTMODE=$CONTONLY
+BTMODE=$BURSTWITHCONT
 
 BURSTIW=500
 CONTIW=4
 
 BURSTSTARTMS=0
 
-# for LOAD in 0.9 ;do
-# 	FLOWFILE="$DUMP_DIR/fcts-single-$TCP-$ALG-$LOAD-$BURST_SIZES-$BURST_FREQ.fct"
-# 	TORFILE="$DUMP_DIR/tor-single-$TCP-$ALG-$LOAD-$BURST_SIZES-$BURST_FREQ.stat"
-# 	./waf --run "simple-noincast-continuous --load=$LOAD --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --serverCount=$SERVERS --spineCount=$SPINES --leafCount=$LEAVES --linkCount=$LINKS --spineLeafCapacity=$LEAF_SPINE_CAP --leafServerCapacity=$SERVER_LEAF_CAP --linkLatency=$LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --statBuf=$STATIC_BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --request=$BURST_SIZE --queryRequestRate=$BURST_FREQ --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --alphaUpdateInterval=$ALPHA_UPDATE_INT --fctOutFile=$FLOWFILE --torOutFile=$TORFILE"
-# done
-
 FLOWFILE="$DUMP_DIR/fcts-single-$TCP-$ALG-$version.fct"
 TORFILE="$DUMP_DIR/tor-single-$TCP-$ALG-$version.stat"
-./waf --run "star-reverse-bt --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatency=$LEAF_SINK_LATENCY --serverLeafLinkLatency=$SERVER_LEAF_LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --continuousAlpha=$CONTA --burstyAlpha=$BURSTA --numContinuousFlows=$NUMCONTFLOWS --numBurstyFlows=$NUMBURSTFLOWS --btMode=$BTMODE --continuousInitialWindow=$CONTIW --burstyInitialWindow=$BURSTIW --burstyStartTime=$BURSTSTARTMS"
+PARAMFILE="$DUMP_DIR/param-single-$TCP-$ALG-$version.txt"
+./waf --run "star-reverse-bt --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatency=$LEAF_SINK_LATENCY --serverLeafLinkLatency=$SERVER_LEAF_LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --continuousAlpha=$CONTA --burstyAlpha=$BURSTA --numContinuousFlows=$NUMCONTFLOWS --numBurstyFlows=$NUMBURSTFLOWS --btMode=$BTMODE --continuousInitialWindow=$CONTIW --burstyInitialWindow=$BURSTIW --burstyStartTime=$BURSTSTARTMS"
