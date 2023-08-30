946a54206c35734424585578d8dd9f098181c752
Ann Zhou <ann.zhou@wustl.edu>
2023-08-30 11:45:50 -0400

    [hetero-rtt] star-hetero-rtt-copy copied from star-hetero-rtt

---
+++ ns-3.35/examples/abm-evaluation/run-star-hetero-rtt-copy.sh
@@ -0,0 +1,74 @@
+
+NS3="/u/az6922/COS513-Project/ns-3.35"
+DIR="$NS3/examples/abm-evaluation"
+DUMP_DIR="/u/az6922/data"
+
+cd $NS3
+
+DT=101
+FAB=102
+CS=103
+IB=104
+ABM=110
+
+RENO=0
+CUBIC=1
+DCTCP=2
+# HPCC=3
+# POWERTCP=4
+# HOMA=5
+# TIMELY=6
+# THETAPOWERTCP=7
+
+# FLOWSIZEMODE
+CONTINUOUS=0
+BURSTY=1
+NONE=2
+
+SERVER_LEAF_CAP=10
+LEAF_SINK_CAP=1
+SERVER_LEAF_LATENCY=1000
+LEAF_SINK_LATENCY_LONG_RTT=20000
+LEAF_SINK_LATENCY_SHORT_RTT=2000
+
+RED_MIN=10
+RED_MAX=10
+
+N_PRIO=3 # Changed this
+
+ALPHAFILE="$DIR/alphas"
+CDFFILE="$DIR/websearch.txt"
+CDFNAME="WS"
+
+NUMSINKS=2
+NUMNODES=110
+
+BUFFER=12500000
+
+START_TIME=1
+END_TIME=51
+FLOW_END_TIME=26
+
+TCP=$CUBIC
+ALG=$DT
+version=2
+
+LONGA=1
+SHORTA=1
+
+NUMLONGFLOWS=10
+NUMSHORTFLOWS=100
+
+FSMODELONGRTT=$NONE
+FSMODESHORTRTT=$CONTINUOUS
+
+SHORTIW=4
+LONGIW=4
+
+LONGSTARTMS=1000
+SHORTSTARTMS=1000
+
+FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-$TCP-$ALG-$version.fct"
+TORFILE="$DUMP_DIR/tor-hetero-rtt-$TCP-$ALG-$version.stat"
+PARAMFILE="$DUMP_DIR/param-hetero-rtt-$TCP-$ALG-$version.txt"
+./waf --run "star-hetero-rtt-copy --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatency=$SERVER_LEAF_LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS"
