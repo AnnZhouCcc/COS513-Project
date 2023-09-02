b696e80d0ce9a0cfd96384ffcfc13529c00b5d30
Ann Zhou <ann.zhou@wustl.edu>
2023-09-02 17:49:33 -0400

    [hetero-rtt] set up bulk run for *-bb-before

--- ns-3.35/examples/abm-evaluation/bulk-run-star-hetero-rtt.sh
+++ ns-3.35/examples/abm-evaluation/bulk-run-star-hetero-rtt.sh
@@ -1,7 +1,7 @@
 
 NS3="/u/az6922/COS513-Project/ns-3.35"
 DIR="$NS3/examples/abm-evaluation"
-DUMP_DIR="/u/az6922/data/hetero-rtt-cc-after-aug30"
+DUMP_DIR="/u/az6922/data/hetero-rtt-bb-before-sep2"
 
 cd $NS3
 
@@ -27,9 +27,10 @@ NONE=2
 
 SERVER_LEAF_CAP=10
 LEAF_SINK_CAP=1
-SERVER_LEAF_LATENCY=1000
-LEAF_SINK_LATENCY_LONG_RTT=20000
-LEAF_SINK_LATENCY_SHORT_RTT=2000
+SERVER_LEAF_LATENCY_LONG_RTT=20000
+SERVER_LEAF_LATENCY_SHORT_RTT=2000
+LEAF_SINK_LATENCY_LONG_RTT=1000
+LEAF_SINK_LATENCY_SHORT_RTT=1000
 
 RED_MIN=10
 RED_MAX=10
@@ -55,42 +56,47 @@ NUMSHORTFLOWS=10
 NUMSINKS=2
 NUMNODES=`expr $NUMLONGFLOWS + $NUMSHORTFLOWS`
 
-SHORTIW=4
-LONGIW=4
+SHORTIW=50
+LONGIW=50
 
-LONGSTARTMS=1000
-SHORTSTARTMS=1000
+LONGSTARTMS=2000
+SHORTSTARTMS=2000
 
-for B in 5 10 15 20 25 30 35 40 45 50; do
+BURSTSTARTRANGENS=5000000
+
+for B in 10 15 20 25 30 35 40 45; do
     BUFFER=`expr $B \* 100000`
-    for seed in {1..11}; do
-        FSMODELONGRTT=$CONTINUOUS
-        FSMODESHORTRTT=$CONTINUOUS
-        FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.fct"
-        TORFILE="$DUMP_DIR/tor-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.stat"
-        PARAMFILE="$DUMP_DIR/param-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.txt"
-        time ./waf --run "star-hetero-rtt --randomSeed=$seed --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatency=$SERVER_LEAF_LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS" &
-        sleep 20
+    for M in {1..5}; do
+        MINBURSTSIZE=expr $M \* 100000000`
+        for seed in {1..11}; do
+            FSMODELONGRTT=$BURSTY
+            FSMODESHORTRTT=$BURSTY
+            FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.fct"
+            TORFILE="$DUMP_DIR/tor-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.stat"
+            PARAMFILE="$DUMP_DIR/param-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.txt"
+            time ./waf --run "star-hetero-rtt --randomSeed=$seed --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatencyLongRTT=$SERVER_LEAF_LATENCY_LONG_RTT --serverLeafLinkLatencyShortRTT=$SERVER_LEAF_LATENCY_SHORT_RTT --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS --minBurstSize=$MINBURSTSIZE --burstStartRange=$BURSTSTARTRANGENS" &
+            sleep 10
+        done
+        wait
+        for seed in {1..11}; do
+            FSMODELONGRTT=$BURSTY
+            FSMODESHORTRTT=$NONE
+            FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.fct"
+            TORFILE="$DUMP_DIR/tor-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.stat"
+            PARAMFILE="$DUMP_DIR/param-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.txt"
+            time ./waf --run "star-hetero-rtt --randomSeed=$seed --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatencyLongRTT=$SERVER_LEAF_LATENCY_LONG_RTT --serverLeafLinkLatencyShortRTT=$SERVER_LEAF_LATENCY_SHORT_RTT --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS --minBurstSize=$MINBURSTSIZE --burstStartRange=$BURSTSTARTRANGENS" &
+            sleep 10
+        done
+        wait
+        for seed in {1..11}; do
+            FSMODELONGRTT=$NONE
+            FSMODESHORTRTT=$BURSTY
+            FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.fct"
+            TORFILE="$DUMP_DIR/tor-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.stat"
+            PARAMFILE="$DUMP_DIR/param-hetero-rtt-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.txt"
+            time ./waf --run "star-hetero-rtt --randomSeed=$seed --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatencyLongRTT=$SERVER_LEAF_LATENCY_LONG_RTT --serverLeafLinkLatencyShortRTT=$SERVER_LEAF_LATENCY_SHORT_RTT --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS --minBurstSize=$MINBURSTSIZE --burstStartRange=$BURSTSTARTRANGENS" &
+            sleep 10
+        done
+        wait
     done
-    #wait
-    for seed in {1..11}; do
-        FSMODELONGRTT=$CONTINUOUS
-        FSMODESHORTRTT=$NONE
-        FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.fct"
-        TORFILE="$DUMP_DIR/tor-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.stat"
-        PARAMFILE="$DUMP_DIR/param-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.txt"
-        time ./waf --run "star-hetero-rtt --randomSeed=$seed --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatency=$SERVER_LEAF_LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS" &
-        sleep 20
-    done
-    #wait
-    for seed in {1..11}; do
-        FSMODELONGRTT=$NONE
-        FSMODESHORTRTT=$CONTINUOUS
-        FLOWFILE="$DUMP_DIR/fcts-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.fct"
-        TORFILE="$DUMP_DIR/tor-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.stat"
-        PARAMFILE="$DUMP_DIR/param-hetero-rtt-cc-after-$BUFFER-$FSMODELONGRTT-$FSMODESHORTRTT-$seed.txt"
-        time ./waf --run "star-hetero-rtt --randomSeed=$seed --StartTime=$START_TIME --EndTime=$END_TIME --FlowLaunchEndTime=$FLOW_END_TIME --numSinks=$NUMSINKS --numNodes=$NUMNODES --leafSinkCapacity=$LEAF_SINK_CAP --serverLeafCapacity=$SERVER_LEAF_CAP --leafSinkLinkLatencyLongRTT=$LEAF_SINK_LATENCY_LONG_RTT --leafSinkLinkLatencyShortRTT=$LEAF_SINK_LATENCY_SHORT_RTT --serverLeafLinkLatency=$SERVER_LEAF_LATENCY --TcpProt=$TCP --BufferSize=$BUFFER --algorithm=$ALG --RedMinTh=$RED_MIN --RedMaxTh=$RED_MAX --nPrior=$N_PRIO --alphasFile=$ALPHAFILE --cdfFileName=$CDFFILE --torOutFile=$TORFILE --fctOutFile=$FLOWFILE --paramOutFile=$PARAMFILE --longRTTAlpha=$LONGA --shortRTTAlpha=$SHORTA --numLongRTTFlows=$NUMLONGFLOWS --numShortRTTFlows=$NUMSHORTFLOWS --fsModeLongRTT=$FSMODELONGRTT --fsModeShortRTT=$FSMODESHORTRTT --longRTTInitialWindow=$LONGIW --shortRTTInitialWindow=$SHORTIW --longRTTStartTime=$LONGSTARTMS --shortRTTStartTime=$SHORTSTARTMS" &
-        sleep 20
-    done
-    wait
 done
